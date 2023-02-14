6036db6bfab6aaf257cb177a5a9d27f5c6096eb2
Ashley Jacobs <amjacobs@ucsd.edu>
2023-02-14 15:03:01 -0800

    Added starter code

---
+++ GradeServer.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+class ExecHelpers {
+
+  /**
+    Takes an input stream, reads the full stream, and returns the result as a
+    string.
+
+    In Java 9 and later, new String(out.readAllBytes()) would be a better
+    option, but using Java 8 for compatibility with ieng6.
+  */
+  static String streamToString(InputStream out) throws IOException {
+    String result = "";
+    while(true) {
+      int c = out.read();
+      if(c == -1) { break; }
+      result += (char)c;
+      System.out.println(System.currentTimeMillis() + "; just read: " + (char)c);
+    }
+    return result;
+  }
+
+  /**
+    Takes a command, represented as an array of strings as it would by typed at
+    the command line, runs it, and returns its combined stdout and stderr as a
+    string.
+  */
+  static String exec(String[] cmd) throws IOException {
+    Process p = new ProcessBuilder()
+                    .command(Arrays.asList(cmd))
+                    .redirectErrorStream(true)
+                    .start();
+    InputStream out = p.getInputStream();
+    return String.format("%s\n", streamToString(out));
+  }
+
+}
+
+class Handler implements URLHandler {
+    public String handleRequest(URI url) throws IOException {
+       if (url.getPath().equals("/grade")) {
+           String[] parameters = url.getQuery().split("=");
+           if (parameters[0].equals("repo")) {
+               return ExecHelpers.exec(new String[]{"bash", "grade.sh", parameters[1]});
+           }
+           else {
+               return "Couldn't find query parameter repo";
+           }
+       }
+       else {
+           return "Don't know how to handle that path!";
+       }
+    }
+}
+
+class GradeServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
+
