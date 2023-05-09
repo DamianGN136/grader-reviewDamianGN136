20837447659f4b2c9068b5ccb7ade360ddb59569
Joe Politz <joe@cs.brown.edu>
2023-05-08 23:16:49 -0700

    grading-area

--- grade.sh
+++ grade.sh
@@ -1,10 +1,19 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
+rm -rf grading-area
+
+mkdir grading-area
+
 git clone $1 student-submission
 echo 'Finished cloning'
 
-cp student-submission/ListExamples.java ./
-javac -cp $CPATH *.java
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+cp student-submission/ListExamples.java ./grading-area
+cp TestListExamples.java ./grading-area
+cp -r lib grading-area
 
+# Draw a picture/take notes on the directory structure that's set up after
+# getting to this point
+
+# Then, add here code to compile and run, and do any post-processing of the
+# tests
