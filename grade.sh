f4c8f2ea1527acfdd22d30aa2fcc13b29c52df55
DamianGN136 <130081740+DamianGN136@users.noreply.github.com>
2023-05-25 09:06:09 -0700

    m

--- grade.sh
+++ grade.sh
@@ -33,7 +33,7 @@ javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
 java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples.java
 
 
-if[[]]
+
 
 
 
